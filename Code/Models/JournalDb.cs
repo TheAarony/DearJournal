babc860e61711ef939e59a8fef54d44bbc245ed4
Petrified799 <A.AaronJosephRejiEapen@lincolnuni.ac.nz>
2026-07-06 20:04:13 +1200

    Updated CreatedAt to automatically get current date

--- Code/Models/JournalDb.cs
+++ Code/Models/JournalDb.cs
@@ -1,11 +1,11 @@
 using System.ComponentModel.DataAnnotations;
 namespace Code.Models;
+using System;
 
 public class JournalDb
 {
     public int Id { get; set; }
-    [ Required ]
-    public string Title { get; set; }
+    public required string Title { get; set; }
     public string? Content { get; set; }
-    public DateTime CreatedAt { get; set; }
+    public DateOnly CreatedAt { get; set; } = DateOnly.FromDateTime(DateTime.Now);
 }
