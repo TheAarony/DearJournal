6388f03aa63610df17497201620ada2592627211
Petrified799 <A.AaronJosephRejiEapen@lincolnuni.ac.nz>
2026-07-06 20:06:12 +1200

    Updated action methods
    1. Index method retrieves all journals in the database
    2. NewJournal method gets the user entered data and saves it to the database.
    3. EditJournal method accepts the changes the user made.
    4. DeleteJournal method that deletes the journals.

--- Code/Controllers/HomeController.cs
+++ Code/Controllers/HomeController.cs
@@ -1,19 +1,63 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Code.Models;
+using Code.Data;
 
 namespace Code.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly ILogger<HomeController> _logger;
+
+    private readonly AppDbContext _context;
+
+    public HomeController(ILogger<HomeController> logger, AppDbContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
     public IActionResult Index()
     {
-        return View();
+        var allJournals = _context.Journals.ToList();
+
+        ViewData["JournalCount"] = _context.Journals.Count();
+        return View(allJournals);
     }
 
     public IActionResult NewJournal()
     {
         return View();
+    }
+
+    public IActionResult NewJournalForm( JournalDb model)
+    {
+        _context.Journals.Add(model);
+        _context.SaveChanges();
+        _logger.LogInformation("New journal created.");
+
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult EditJournal(JournalDb model)
+    {
+        //editing ->loading the journal to edit
+        _context.Journals.Update(model);
+        _context.SaveChanges();
+        _logger.LogInformation("Journal edited.");
+
+        var journalinDb = _context.Journals.SingleOrDefault(entry => entry.Id == model.Id);
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult DeleteJournal(JournalDb model)
+    {
+        var journalinDb = _context.Journals.SingleOrDefault(entry => entry.Id == model.Id);
+        _context.Journals.Remove(journalinDb);
+        _context.SaveChanges();
+
+        _logger.LogInformation("Journal deleted.");
+
+        return RedirectToAction("Index");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
